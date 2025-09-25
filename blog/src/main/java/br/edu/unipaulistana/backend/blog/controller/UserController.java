f9b6c7c2dccbb828e93a93dbaff1f3be206caa51
Gabrielvitorf6 <gabrielvitorf6@gmail.com>
2025-09-25 19:17:28 -0300

    Added: Test file: Integration test
    ResponseEntity added in all methods on Controller to better HTTP responses

--- blog/src/main/java/br/edu/unipaulistana/backend/blog/controller/UserController.java
+++ blog/src/main/java/br/edu/unipaulistana/backend/blog/controller/UserController.java
@@ -3,6 +3,8 @@ package br.edu.unipaulistana.backend.blog.controller;
 import br.edu.unipaulistana.backend.blog.domainmodel.User;
 import br.edu.unipaulistana.backend.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,7 +12,7 @@ import java.util.UUID;
 
 //http://localhost:8080/users
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 
 public class UserController {
@@ -18,43 +20,52 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<User> findAll() {
-        return userService.findAll();
-        //use debug action in this celula and acess via browser previous link, to show all users
+    public ResponseEntity<List<User>> findAll() {
+       return   ResponseEntity.ok(this.userService.findAll());
+        //use debug action in this celula and access via browser previous link, to show all users
         //this is our first endpoint, is a web service that request something and response something
     }
+
     //htt://localhost:8080/users/{id}
     @GetMapping("/{id}")
-    public User findUserbyID(@PathVariable UUID id) {
+    public ResponseEntity<User> findUserbyID(@PathVariable UUID id) {
         //public user indicates that only one user is returned
         //@PathVariable indicates that is a variable from URL
         //spring is going to understand that is a variable and will search in method search
         //this variable will be placed in URL after /users
 
-        return this.userService.findbyID(id);
+        //Now doing integration tests, we see that HTTP response is 200(Ok response) and not 201(Created response)
+        //To solve this we use ResponseEntity class, that show us a full HTTP response send to client when this method is invocated
+        //Using this is important when we create end points, to send full responses to various cases
+        return ResponseEntity.ok(this.userService.findbyID(id));
     }
+
     //Indicates that function is going to be requested via delete
     //htt://localhost:8080/users/{id}
     //Delete anotation to delete objects or information in spring
     @DeleteMapping("/{id}")
-    public void deleteUserbyID(@PathVariable UUID id){
+    public ResponseEntity<Void> deleteUserbyID(@PathVariable UUID id) {
         this.userService.deleteByID(id);
+        return ResponseEntity.notFound().build();
+        //We don't return anything because this method only delete, so we can delete normally but nothing can be returned
     }
+
     //Indicates that function is going to be requested via post
     //Method to create user
     @PostMapping
-    public User createUser(@RequestBody User user){
-        return this.userService.create(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(this.userService.create(user), HttpStatus.CREATED);
     }
 
+    //We create a user normally, but we don't pass anything through ResponseEntity, we just say that it's a create http request
     @PutMapping
-    public User updateUser(@RequestBody User user){
-        return this.userService.update(user);
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        return new ResponseEntity<>(this.userService.create(user), HttpStatus.CREATED);
     }
 
     @PatchMapping
-    public User patchUser(@RequestBody User user){
-        return this.userService.partUpdate(user);
+    public ResponseEntity<User> patchUser(@RequestBody User user){
+        return new ResponseEntity<>(this.userService.create(user), HttpStatus.CREATED);
     }
 
     }
---
+++ blog/src/test/br/edu/unipaulistana/backend/blog/UserControllerIntegrationTest.java
@@ -0,0 +1,73 @@
+package br.edu.unipaulistana.backend.blog;
+
+import br.edu.unipaulistana.backend.blog.controller.UserController;
+import br.edu.unipaulistana.backend.blog.domainmodel.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+//Test file
+//We need to write @SpringBootTest annotation to inform spring that is a test class
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//Random port deploy test in a random port, not the 8080 port we always use
+
+@AutoConfigureMockMvc//
+public class UserControllerIntegrationTest {
+
+    //We test all endpoints and folders, and it's called "Integration test", when we integrate all system at once and test
+    //@Autowired = annotation to inject all classes
+    @Autowired
+    private UserController userController;
+
+   @Autowired
+    private MockMvc mockMvc;
+   //Create false HTTPS requests
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    //Object mapper pass object to json and json to object
+
+    @Test //We need to declare this method as method test //We use annotation @Test
+
+    //We need a annotation to see how it's works inside IDE and describe it
+    @DisplayName("Full flow : POST -> GET ALL -> GET_BY_ID -> PUT -> PATH -> GET -> ERROR404")
+    public void fullFlow() throws Exception{
+        UUID id = UUID.randomUUID();//Attributes id variable as UUID random id
+
+        //We can use var type variable when we inside a method and using a class that don't have ambiguous
+        //Here we pass all parameters from a user through a body
+        var bodyCreate = new User(id, "Gabriel", "gabriel@gmail.com", "kurioso", null, null);
+        var postResult = mockMvc.perform(        //perform an action
+                        post("/api/users")
+                                .contentType(MediaType.APPLICATION_JSON)//content type -> mediatype -> APPLICATION_JSON
+                                //Now we inform what content is inside our body
+                                //And we pass the object that is written in Java and pass it to Json, and we use objectMapper class to do it(Object to Json and Json to object)
+                                .content(objectMapper.writeValueAsString(bodyCreate)))//objectMapper convert our object(bodyCreate) to String value
+                .andExpect(status().isCreated())//Expect is used to verify if a request was generated in right way, in our case, 201 request(Created)
+
+                .andExpect(jsonPath("$.id", is(id)))//Expect my object.id = id
+                // Json path is a consult language for json,used here to navigate trough json response object
+                //$ character is used to start navigation in json root, in this case, we say "Go to json root, and search id property and return its value
+
+                .andExpect(jsonPath("$.name", is("Gabriel")))//Expect my object.name = name
+                .andReturn();//Invocate and verify if result is the same as expected
+
+        var created = objectMapper.readValue(postResult.getResponse().getContentAsByteArray(), User.class);//This method need a signature class, declared as .class
+        var returnedId = created.getId();
+
+
+    }
+}
